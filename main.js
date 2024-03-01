c2e0f335096c886cc33cc4c2d89437a6f36eab56
diaarroa <161786007+diaarroa@users.noreply.github.com>
2024-02-29 17:42:54 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function darkmode(){
+    document.getElementById("q").style.backgroundColor="black";
+    document.getElementById("q").style.color="white";
+    document.getElementById("title").style.color="lightblue";
+    document.getElementById("pic1").style.borderColor="rgb(164, 239, 245)";
+}
+
+function lightmode(){
+    document.getElementById("q").style.backgroundColor="white";
+    document.getElementById("q").style.color="black";
+    document.getElementById("title").style.color="navy";
+    document.getElementById("pic1").style.borderColor="blue";
+}
